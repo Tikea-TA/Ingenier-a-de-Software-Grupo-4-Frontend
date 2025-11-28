6ff724ffb8f4ae2876edd033656b50d5650cd074
apc <apicoyc@gmail.com>
2025-11-28 17:13:41 -0500

    Cambiar de pdf a zip para documentacion de locales, productores y eventos

--- src/api/eventoService.js
+++ src/api/eventoService.js
@@ -98,7 +98,7 @@ export const descargarDocumentoEvento = async (idEvento, nombreEvento) => {
     const url = window.URL.createObjectURL(new Blob([response.data]));
     const link = document.createElement('a');
     link.href = url;
-    link.setAttribute('download', `Sustento_${nombreEvento}.pdf`);
+    link.setAttribute('download', `Sustento_${nombreEvento}.zip`);
     document.body.appendChild(link);
     link.click();
     link.remove();
